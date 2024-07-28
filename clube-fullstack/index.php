6b8257dba5c3cbaf459bd9f6b1d9ed6c0375a7da
Christian <christiannmenezes@gmail.com>
2024-07-28 17:39:04 -0300

    exception handling

--- clube-fullstack/index.php
+++ clube-fullstack/index.php
@@ -27,4 +27,49 @@ class Twig extends Template
   }
 }
 
-echo (new Twig)->load();
+// echo (new Twig)->load();
+
+/**
+ * Exceptions
+ */
+
+class TestException extends Exception
+{
+  public function message(): string
+  {
+    // set default timezone
+    date_default_timezone_set('America/Sao_Paulo');
+
+    // get the current date & time
+    $time = date('F j, Y, g:i a e O');
+
+    // format the exception message
+    $message = "[{$time}]: Erro no arquivo {$this->getFile()} na linha {$this->getLine()} com a mensagem: {$this->getMessage()}\n";
+
+    // append to the error log
+    error_log($message, 3, 'app/logs/errors.log');
+
+    return "Um erro inesperado aconteceu!";
+  }
+
+  public function __toString(): string
+  {
+    return "Um erro inesperado aconteceu!";
+  }
+}
+
+class Test
+{
+  public function methodTest()
+  {
+    throw new TestException('erro teste');
+  }
+}
+
+try {
+  $test = new Test;
+
+  $test->methodTest();
+} catch (TestException $e) {
+  echo "Exception: {$e}";
+}
